add3061b30e3ca66729d8f9fa5e00283ee065953
MarCodeson <omardixon@hotmail.co.uk>
2025-10-27 23:14:39 +0000

    Updated site.config with logo and testimonial assets

--- content/site.config.ts
+++ content/site.config.ts
@@ -1,8 +1,9 @@
 export const site = {
-  brand: {
-    name: 'LH Chiropractic',
-    gold: '#C9A646' // temp until sampled from SVG
-  },
+ brand: {
+  name: 'LH Chiropractic',
+  logo: 'https://new.express.adobe.com/webpage/bqeX7Q23jt49u/resources/c3b0f59b-538a-44c3-b67e-33e25e231379?asset_id=c3b0f59b-538a-44c3-b67e-33e25e231379&size=2560',
+  gold: '#C9A646'
+},
   contacts: {
     email: 'drleshall@aol.com',
     phoneUK: '+44 07715 168864',
@@ -31,4 +32,14 @@ export const site = {
     { name: 'GCC Registered No. 00616' },
     { name: 'AECC University College Graduate' },
   ],
+testimonials: [
+{ img: 'https://new.express.adobe.com/webpage/bqeX7Q23jt49u/resources/c5b871e6-25ea-48a8-b4a8-2ebc9112b950?asset_id=c5b871e6-25ea-48a8-b4a8-2ebc9112b950&size=1024', quote: '“Testimonial 1 text to transcribe later.”', author: 'Patient 1' },
+{ img: 'https://new.express.adobe.com/webpage/bqeX7Q23jt49u/resources/c6f93b51-bab1-4540-89be-f0dbd96e4116?asset_id=c6f93b51-bab1-4540-89be-f0dbd96e4116&size=1024', quote: '“Testimonial 2 text to transcribe later.”', author: 'Patient 2' },
+{ img: 'https://new.express.adobe.com/webpage/bqeX7Q23jt49u/resources/a1a78909-63ad-4a0c-898e-329fd9bac8fc?asset_id=a1a78909-63ad-4a0c-898e-329fd9bac8fc&size=1024', quote: '“Testimonial 3 text to transcribe later.”', author: 'Patient 3' },
+{ img: 'https://new.express.adobe.com/webpage/bqeX7Q23jt49u/resources/d545be01-0426-4897-b12c-8fa6c146d41c?asset_id=d545be01-0426-4897-b12c-8fa6c146d41c&size=1024', quote: '“Testimonial 4 text to transcribe later.”', author: 'Patient 4' },
+{ img: 'https://new.express.adobe.com/webpage/bqeX7Q23jt49u/resources/6c0f363a-a82f-462d-bf2d-54c0697c6821?asset_id=6c0f363a-a82f-462d-bf2d-54c0697c6821&size=1024', quote: '“Testimonial 5 text to transcribe later.”', author: 'Patient 5' },
+{ img: 'https://new.express.adobe.com/webpage/bqeX7Q23jt49u/resources/8890af99-892b-4d9a-926d-af5ec90db233?asset_id=8890af99-892b-4d9a-926d-af5ec90db233&size=1024', quote: '“Testimonial 6 text to transcribe later.”', author: 'Patient 6' },
+{ img: 'https://new.express.adobe.com/webpage/bqeX7Q23jt49u/resources/bf2a20f0-fdfa-4e23-8520-cbacad3f99d1?asset_id=bf2a20f0-fdfa-4e23-8520-cbacad3f99d1&size=1024', quote: '“Testimonial 7 text to transcribe later.”', author: 'Patient 7' },
+{ img: 'https://new.express.adobe.com/webpage/bqeX7Q23jt49u/resources/45fe50ca-4edc-4e1a-8076-e192791e91fc?asset_id=45fe50ca-4edc-4e1a-8076-e192791e91fc&size=1024', quote: '“Testimonial 8 text to transcribe later.”', author: 'Patient 8' }
+]
 } as const
